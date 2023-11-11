00dc0f44a24f7144790a5004fb730beac53a225d
VietTien02 <106070479+VietTien02@users.noreply.github.com>
2023-11-11 07:27:44 +0700

    kiem tra

---
+++ KiemTra/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using KiemTra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiemTra.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Cau1()
+        {
+           
+            return View();
+        }
+        public ActionResult Cau2()
+        {
+           
+
+            return View();
+        }
+        public ActionResult KQ(Teacher t)
+        {
+            t.maGv = Request["magv"];
+            t.Hoten = Request["hoten"];
+
+            string temp = "";
+            if (Request["Nam"] == "true, false")
+                temp = "Nam";
+            else
+                temp += " " + " Nu";
+            t.sex = temp;
+
+            if (t.SoGio < 100)
+            {
+                t.luong = t.SoGio * t.LuongGio;
+            }
+            else if (t.SoGio < 200)
+            {
+                t.luong = t.SoGio * t.LuongGio * 1.1;
+            }
+            else
+            {
+                t.luong = t.SoGio * t.LuongGio * 1.2;
+            }
+            return View(t);
+        }
+    }
+}
---
+++ KiemTra/Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KiemTra.Models
+{
+    public class Teacher
+    {
+        public string maGv { get; set; }
+        public string Hoten { get; set; }
+        public string TrinhDo { get; set; }
+        public double LuongGio { get; set; }
+        public string sex { get; set; }
+        public int SoGio { get; set; }
+        public double luong { get; set; }
+    }
+}
